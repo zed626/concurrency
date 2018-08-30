b320c42fa57486b9663e3f4e9d20b50c183e3a3c
zed626 <425719071@qq.com>
2018-08-30 12:29:38 +0800

    init

---
+++ src/main/java/com/example/demo/ConcarrencyApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcarrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcarrencyApplication.class, args);
+	}
+}

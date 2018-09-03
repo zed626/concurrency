f88b7bef7805ac744818340783878c708d5dcccd
zed626 <425719071@qq.com>
2018-09-03 11:49:51 +0800

    JUCbasic

--- src/main/java/com/zed/demo/ConcurrencyApplication.java
+++ src/main/java/com/zed/demo/ConcurrencyApplication.java
@@ -2,11 +2,31 @@ package com.zed.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class ConcurrencyApplication {
+public class ConcurrencyApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConcurrencyApplication.class, args);
 	}
+	
+	@Bean
+	public FilterRegistrationBean<HttpFilter> httpFilter() {
+		FilterRegistrationBean<HttpFilter> registrationBean=new FilterRegistrationBean<HttpFilter>();
+		registrationBean.setFilter(new HttpFilter());
+		registrationBean.addUrlPatterns("/threadLocal/*");
+		return registrationBean;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		// TODO Auto-generated method stub
+		registry.addInterceptor(new HttpInterceptor()).addPathPatterns("/**");
+	}
+	
+	
 }
